e32b5938aae57089560a11d7a85a788feb3dde48
Thangavel p <thangavel.p2023mech@sece.ac.in.com>
2025-07-07 19:37:29 +0530

    Initial commit

---
+++ Springbootfirst/src/main/java/com/example/Springbootfirst/controllers/HelloworldController.java
@@ -0,0 +1,59 @@
+package com.example.Springbootfirst.controllers;
+
+import com.example.Springbootfirst.models.Employee;
+import com.example.Springbootfirst.services.HelloWorldService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/employee")
+public class HelloworldController {
+
+    @Autowired
+    HelloWorldService hws;
+
+    @GetMapping
+    public String hello() {
+        return hws.GetMethod();
+    }
+
+    @GetMapping("/all")
+    public List<Employee> getAllEmployees() {
+        return hws.getAllEmployees();
+    }
+
+    @GetMapping("/{empid}")
+    public Employee getEmployeeById(@PathVariable int empid) {
+        return hws.getEmployeeById(empid);
+    }
+
+    @PostMapping
+    public String postMethod(@RequestBody Employee employee) {
+        return hws.postMethod(employee);
+    }
+
+    @PostMapping("/add")
+    public String addEmployee(@RequestBody Employee employee) {
+        hws.addEmployee(employee);
+        return "Employee added successfully";
+    }
+
+    @PutMapping("/{empid}")
+    public String putMethod(@PathVariable int empid, @RequestBody Employee employee)
+    {
+        return hws.updateRecords(empid, employee);
+    }
+
+
+    @DeleteMapping
+    public String deleteMethod() {
+        return hws.Deletemethod();
+    }
+
+    @DeleteMapping("/{empid}")
+    public String deleteEmployee(@PathVariable int empid) {
+        return hws.deleteEmployeeById(empid);
+    }
+}
